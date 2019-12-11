9bc3a60e2672e76f376f7709d04c1d96dcb99c4f
Hugh Saunders <hugh@wherenow.org>
2019-12-11 23:16:15 +0000

    use glob to find sensor

--- temp.py
+++ temp.py
@@ -1,5 +1,6 @@
 import datetime
 import json
+import glob
 from flask import Flask
 
 app = Flask(__name__)
@@ -9,10 +10,9 @@ def getTemp():
     # Sample data
     # 4e 01 55 05 7f a5 81 66 78 : crc=78 YES
     # 4e 01 55 05 7f a5 81 66 78 t=20875
-    file="/sys/bus/w1/devices/28-03129779979d/w1_slave"
+    file=glob.glob('/sys/bus/w1/devices/28*')[0]+"/w1_slave"
     with open(file) as f:
         temp=float(f.readlines()[1][29:34])/1000
-        #temp = 21
         return json.dumps({"temp": temp, 
                            "unit": "celsius",
                            "timestamp":datetime.datetime.now().isoformat()})
